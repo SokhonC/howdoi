0a93a2035b812b9ffc41f61785345682efcc68c3
Sokhon Chhun <47809100+SokhonC@users.noreply.github.com>
2022-11-29 23:44:25 +0700

    Update settings.py

--- bot/settings.py
+++ bot/settings.py
@@ -3,7 +3,7 @@ import os
 # load_dotenv()
 
 # BOT_TOKEN = os.getenv('BOT_TOKEN')
-BOT_TOKEN = '5927454928:AAFBlbOyRVnXVW59XwsSzMubmEOp4YIWp7Q'
+BOT_TOKEN = ''
 if not BOT_TOKEN:
     print('You have forgot to set BOT_TOKEN')
     quit()
